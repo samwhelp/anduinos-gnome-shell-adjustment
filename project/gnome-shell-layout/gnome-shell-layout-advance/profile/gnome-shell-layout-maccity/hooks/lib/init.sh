cb035e9563cd2dd0087309d69e5ac56a2c167abd
samwhelp <samwhelp@users.noreply.github.com>
2026-01-11 22:08:11 +0800

    fix

--- project/gnome-shell-layout/gnome-shell-layout-advance/profile/gnome-shell-layout-maccity/hooks/lib/init.sh
+++ project/gnome-shell-layout/gnome-shell-layout-advance/profile/gnome-shell-layout-maccity/hooks/lib/init.sh
@@ -5,6 +5,11 @@ DEFAULT_IS_DEBUG=false
 IS_DEBUG="${IS_DEBUG:=$DEFAULT_IS_DEBUG}"
 
 
+## option
+DEFAULT_ONLY_SWITCH=false
+ONLY_SWITCH="${ONLY_SWITCH:=$DEFAULT_ONLY_SWITCH}"
+
+
 ## core
 . "${plan_dir_path}/lib/core.sh"
 
